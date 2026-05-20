f8c662d6492c14639f95949413c37ba976186e29
wyxHub <wyx.me@outlook.com>
2026-05-21 00:59:43 +0800

    Fix addition

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.get('/health', (req, res) => {
 app.get('/add', (req, res) => {
   const a = Number(req.query.a);
   const b = Number(req.query.b);
-  res.json({ result: a - b });
+  res.json({ result: a + b });
 });
 
 module.exports = app;
